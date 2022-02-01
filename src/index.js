7ee6fbf257074a4c40ae38484a90c9f452f6db0c
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-02-01 11:06:06 -0300

    Anexando Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.scss';
+
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const App = () => {
     return (
-        <h1>Project with React and Webpack</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Project with React and Webpack</h1>
+                    <div className="alert alert-danger" role="alert">
+                        esto es bootstrap
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
